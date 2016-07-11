1d024b050c3dea308c9f27f3f4df8ec465745e0a
noahpatterson <n@noahpatterson.me>
2016-07-10 22:14:37 -0400

    add color sliders to change the color

--- ClickCounter/ColorMakerViewController.swift
+++ ClickCounter/ColorMakerViewController.swift
@@ -15,6 +15,10 @@ class ColorMakerViewController: UIViewController {
     @IBOutlet weak var blueSwitch: UISwitch!
     @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +30,13 @@ class ColorMakerViewController: UIViewController {
         let r: CGFloat = self.redSwitch.on   ? 1 : 0
         let g: CGFloat = self.greenSwitch.on ? 1 : 0
         let b: CGFloat = self.blueSwitch.on  ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
+    }
+    @IBAction func changeColorWithSlider(sender: AnyObject) {
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
